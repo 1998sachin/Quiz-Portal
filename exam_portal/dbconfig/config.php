5c1015287b09bd302c673ef911fd70a5aff414aa
Srijan Prakash <30442831+SrijanPrakash@users.noreply.github.com>
2018-07-02 01:12:46 +0530

    Update config.php

--- exam_portal/dbconfig/config.php
+++ exam_portal/dbconfig/config.php
@@ -1,4 +1,4 @@
 <?php
-    $con = mysqli_connect("localhost","root","shivani1995")  or die("Unable to connect");
+    $con = mysqli_connect("localhost","root")  or die("Unable to connect");
     mysqli_select_db($con,'project');
 ?>
